285a642aa47ab84cbc145e7f65e44f8e73ae6e52
Tanya <ta_ledkova@bk.ru>
2019-02-07 13:12:05 +0400

    Второй коммит

--- js/script.js
+++ js/script.js
@@ -31,7 +31,57 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+//Таймер
+let deadline = '2019-02-08';
 
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime)-Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+        
+        //Красивый формат таймера 03:02:09
+        if (seconds < 10) {
+            seconds = '0' + Math.floor((t/1000) % 60);
+        }
+        if (minutes < 10) {
+            minutes = '0' + Math.floor((t/1000/60) % 60);
+        }
+        if (hours < 10) {
+            hours = '0' + Math.floor((t/(1000*60*60)));
+        }
+        
+        //Если акция закончилась, то таймер показывает 00:00:00
+        if (t < 0) {
+            seconds = '00';
+            minutes = '00';
+            hours = '00';
+        }
 
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes': minutes,
+            'seconds' : seconds
+        };
+}
 
+function setClock(id,endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.seconds;
+        if (t.total<=0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+setClock('timer', deadline);
 });
